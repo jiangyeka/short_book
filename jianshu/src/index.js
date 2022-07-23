2f2409da6a32fa96870825992ccd7449a72aa755
jiangyeka <940495614@qq.com>
2022-07-23 08:46:42 +0800

    feat -g 初始化改项目

--- jianshu/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style';
-import App from './App';
-import "./statics/iconfont/iconfont.css"
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
